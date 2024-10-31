06120f5c5c95086384e977ad6ddba21e09df3f64
Beatriz Maia <149596621+beatrizmaiar@users.noreply.github.com>
2024-10-31 17:08:30 -0300

    inicio FE cadastro de usuarios

---
+++ swapp_fe/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <CadastroUsuario />
+  </div>
+</template>
+
+<script>
+import CadastroUsuario from "./components/cadastro_usuario.vue";
+
+export default {
+  name: "App",
+  components: {
+    CadastroUsuario,
+  },
+};
+</script>
+
+<style>
+
+body {
+  margin: 0; 
+  padding: 0; 
+  height: 100vh;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  }
+
+</style>
---
+++ swapp_fe/src/components/cadastro_usuario.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+  <nav>
+    <img src="@/assets/logo2.png" alt="Logo" class="logo" />
+  </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "CadastroUsuario",
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #14241F; 
+  display: flex; 
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+
+.logo {
+  width: 200px; 
+  height: auto; 
+}
+</style>
