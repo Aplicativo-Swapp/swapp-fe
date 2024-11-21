1c6326ac938a816b1f8216545e0346866f213d5d
Beatriz Maia <149596621+beatrizmaiar@users.noreply.github.com>
2024-11-20 22:11:56 -0300

    att app.vue e homepage

--- swapp_fe/src/App.vue
+++ swapp_fe/src/App.vue
@@ -19,14 +19,29 @@ export default {
 
 <style>
 
+@font-face {
+  font-family: 'Open Sauce One';
+  src: url('@/assets/fonts/OpenSauceOne-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'Open Sauce One';
+  src: url('@/assets/fonts/OpenSauceOne-Light.ttf') format('truetype');
+  font-weight: 200;
+  font-style: normal;
+}
+
 body {
   margin: 0; 
   padding: 0; 
   height: 100vh;
+  font-family: 'Open Sauce One', sans-serif;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sauce One', sans-serif; /* Define a fonte para todo o app */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- swapp_fe/src/components/home_page.vue
+++ swapp_fe/src/components/home_page.vue
@@ -9,12 +9,26 @@
             v-model="searchQuery"
             placeholder="Pesquise por categorias, localização..."
           />
-          <button @click="search"></button>
+          <button @click="search">buscar</button>
         </div>
 
         <ul class="utility-list">
           <li class="nav-item">
-            <a href="/">Categorias</a>
+            <a href="/"></a>
+            <select v-model="Categorias" @change="categoryChanged">
+            <option value="" disabled selected style="color: white;">Categorias</option>
+            <option value="assistencia">Assistência técnica</option>
+            <option value="aulas">Aulas</option>
+            <option value="autos">Autos</option>
+            <option value="consultoria">Consultoria</option>
+            <option value="tecnologia">Design e Tecnologia</option>
+            <option value="eventos">Eventos</option>
+            <option value="moda">Moda e beleza</option>
+            <option value="reforma">Reforma e reparos</option>
+            <option value="saude">Saúde</option>
+            <option value="servicos">Serviços Domésticos</option>
+          </select>
+
           </li>
           <li class="nav-item">
             <a href="/">Contato</a>
@@ -36,12 +50,16 @@ export default {
   name: "HomePage",
   data() {
     return {
-      searchQuery: "", //armazena a pesquisa
+      searchQuery: "", // armazena a pesquisa
+      selectedCategory: "", // armazena a categoria selecionada
     };
   },
   methods: {
     search() {
       alert(`Você pesquisou por: ${this.searchQuery}`);
+    },
+    categoryChanged() {
+      alert(`Categoria selecionada: ${this.selectedCategory}`);
     },
   },
 };
@@ -49,6 +67,7 @@ export default {
 
 
 <style scoped>
+
 
 .header {
   background-color: #14241f;
@@ -67,29 +86,34 @@ export default {
 }
 
 .logo {
-  width: 150px;
+  width: 200px;
+  margin-left: 50px;
   height: auto;
+  margin-right: 350px;
 }
 
-/* Campo de Pesquisa */
 .search-bar {
   display: flex;
+  justify-content: center; /* Centraliza horizontalmente */
   align-items: center;
-  gap: 5px;
+  background-color: #eaeaea;
+  border-radius: 8px;
+  width: 350px; /* Tamanho total da barra de pesquisa */
 }
 
 .search-bar input {
-  padding: 8px;
+  padding: 15px 10px;
   border: none;
-  border-radius: 4px;
+  border-radius: 10px 0 0 10px; /* Cantos arredondados para o lado esquerdo */
   outline: none;
-  width: 200px;
+  width: 100%; /* Use uma porcentagem para ajustar proporcionalmente */
+  box-sizing: border-box;
 }
 
 .search-bar button {
-  padding: 8px 10px;
+  padding: 15px 15px;
   border: none;
-  border-radius: 4px;
+  border-radius: 0 10px 10px 0;
   background-color: #89ffdb;
   cursor: pointer;
 }
@@ -99,11 +123,14 @@ export default {
 }
 
 .utility-list {
+  font-family: 'Open Sauce One', sans-serif;
+  font-weight: 200;
   list-style: none;
   display: flex;
   gap: 5px;
   margin: 0;
   padding: 0;
+  margin-left: 300px;
 }
 
 .utility-list li a {
@@ -115,4 +142,19 @@ export default {
 .utility-list li a:hover {
   color: #89ffdb;
 }
+
+.utility-list select {
+  padding: 8px;
+  border-radius: 5px;
+  border: none;
+  margin-left: 10px; 
+  background-color: #14241F;
+  font-size: 14px;
+}
+
+.utility-list select:focus {
+  outline: none;
+  background-color: #eaeaea;
+}
+
 </style>
