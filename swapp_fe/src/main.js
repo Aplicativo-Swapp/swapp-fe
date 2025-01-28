7df8ceb0c98087b7e3b35b7db7e9de3bab026c89
Felipe Carvalho <carvalho.felipe@unifesp.br>
2025-01-28 17:00:30 -0300

    Removendo a pasta 'swapp_fe' da branch prd

--- swapp_fe/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index.js'; // Importa o roteador configurado
-
-const app = createApp(App);
-
-app.use(router); // Integra o roteador ao app Vue
-app.mount('#app'); // Monta o app no elemento HTML
